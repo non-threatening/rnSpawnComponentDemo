18bb6576f0f971282b3ac92341471672758827f3
non-threatening <jerkmo@gmail.com>
2020-10-04 00:10:44 +0200

    init

---
+++ App.js
@@ -0,0 +1,90 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  StatusBar,
+  Text,
+  View,
+} from 'react-native';
+// import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+// import {faCoffee} from '@fortawesome/free-solid-svg-icons';
+
+import {MyComponent} from './MyComponent';
+import {
+  SpawnProvider,
+  SpawnController,
+  AddSpawn,
+  ClearSpawns,
+  RemoveAll,
+} from 'rn-spawn-component';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SpawnProvider>
+        <SafeAreaView>
+          <View style={styles.body}>
+            <ScrollView
+              contentInsetAdjustmentBehavior="automatic"
+              style={styles.scrollView}>
+              <SpawnController
+                startCount={2}
+                spawn={<MyComponent />}
+                name="myComponent"
+              />
+
+              <SpawnController
+                startCount={1}
+                name="text"
+                spawn={
+                  <View>
+                    <Text>Simple inline spawn..</Text>
+                  </View>
+                }
+              />
+            </ScrollView>
+
+            <View>
+              <View style={styles.row}>
+                <AddSpawn name="text" label="Add Text" />
+                <ClearSpawns name="text" label="Clear Text" />
+              </View>
+
+              <View style={styles.row}>
+                <AddSpawn name="myComponent" label="Spawn Component" />
+                <ClearSpawns name="myComponent" label="Clear Spawns" />
+              </View>
+
+              <RemoveAll
+                // prepend={<FontAwesomeIcon icon={faCoffee} />}
+                label=" them "
+                append={<Text>all</Text>}
+                style={{backgroundColor: 'thistle', borderRadius: 7}}
+                textStyle={{color: 'red', fontSize: 28}}
+              />
+            </View>
+          </View>
+        </SafeAreaView>
+      </SpawnProvider>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+  },
+  body: {
+    backgroundColor: '#666',
+    height: '100%',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
---
+++ MyComponent.js
@@ -0,0 +1,21 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Text, View} from 'react-native';
+import {RemoveSpawn} from 'rn-spawn-component';
+
+export const MyComponent = (props) => {
+  const spawnNum = props.spawnNumber;
+  return (
+    <View
+      style={{
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+        margin: 10,
+        height: 80,
+      }}>
+      <View style={{alignItems: 'center'}}>
+        <Text>{spawnNum}</Text>
+        <RemoveSpawn spawn={spawnNum} label={'Kill Me'} />
+      </View>
+    </View>
+  );
+};
